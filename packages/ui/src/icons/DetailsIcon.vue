e8748e0e7b38660aca2471e5990c01cbcd0bb00e
AzadehMn <38600349+azadehmn@users.noreply.github.com>
2026-09-14 17:51:16 +0330

    feat(ui): add icon

---
+++ packages/ui/src/icons/DetailsIcon.vue
@@ -0,0 +1,25 @@
+<template>
+  <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+    <path
+      d="M14.5 3.5H8C6.895 3.5 6 4.395 6 5.5V18.5C6 19.605 6.895 20.5 8 20.5H16C17.105 20.5 18 19.605 18 18.5V7.5L14.5 3.5Z"
+      stroke="currentColor"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M14 3.5V7.5H18"
+      stroke="currentColor"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M9 11.5H15M9 15H13"
+      stroke="currentColor"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
+</template>
